20374b3ab1dd1d9662d639574de0b436215ce702
Rachel <rachel.boye@icloud.com>
2022-08-01 05:08:55 -0400

    part one done

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,16 @@
 
 it('should calculate the monthly rate correctly', function () {
-    // ...
-  });
+    const values = {
+        loanAmount: 8000, totalYears: 5, inRate: 2.0
+      }; 
+      expect(monthlyPayment(values)).toEqual('140.22');
+    });
   
   
   it("should return a result with 2 decimal places", function() {
-    // ..
+    const values = {
+        loanAmount: 8000, totalYears: 8, inRate: 5.708
+      };
+      expect(monthlyPayment(values)).toEqual('104.00');
   });
   
-  /// etc
